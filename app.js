24b248c8da95936708ae704e7f137b8d923682c7
Dasol Park <dasol.park615@gmail.com>
2020-08-18 23:09:12 +0900

    Enums

--- app.js
+++ app.js
@@ -14,39 +14,32 @@
 //   hobbies: ['Sports', 'Cooking'],
 //   role: [2, 'author'],
 // };
-
 // const ADMIN = 0;
 // const READ_ONLY = 1;
 // const AUTHOR = 2;
-
-enum Role {
-  ADMIN = 'ADMIN',
-  READ_ONLY = 100,
-  AUTHOR = 'AUTHOR',
-}
-
-const person = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  role: Role.ADMIN,
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role[Role["READ_ONLY"] = 100] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
 };
-
 // person.role.push('admin'); // 여기서는 개수를 잡아내지 못 한다(주의)
 // person.role[1] = 10;
-
 // person.role = [0, 'admin', 'user']; // 여기서는 개수를 잡아낸다
-
-let favoriteActivities: string[]; // any[]
+var favoriteActivities; // any[]
 favoriteActivities = ['Sports'];
-
 console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase()); // string이기 때문에 string 관련 메소드 사용 가능
-  // console.log(hobby.map()); // !!! ERROR !!!
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase()); // string이기 때문에 string 관련 메소드 사용 가능
+    // console.log(hobby.map()); // !!! ERROR !!!
 }
-
 if (person.role === Role.AUTHOR) {
-  console.log('is author');
+    console.log('is author');
 }
