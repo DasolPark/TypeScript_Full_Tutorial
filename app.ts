4651d6639d420af01f9ba61eb0a8fdb03a565f11
Dasol Park <dasol.park615@gmail.com>
2020-08-18 22:44:44 +0900

    Object Types

--- app.ts
+++ app.ts
@@ -1,20 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-  //   throw new Error('Incorrect input!');
-  // }
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
+const person: {
+  name: string;
+  age: number;
+} = {
+  // This is just TypeScript's representation of an object type that helps TypeScript understand objects are working with
+  // const person = { // This is the better syntax code
+  name: 'Maximilian',
+  age: 30,
+};
 
-let number1: number;
-number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-let resultPhrase = 'Result is: ';
-resultPhrase = 0;
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
