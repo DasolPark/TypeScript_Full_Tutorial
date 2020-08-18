cbfb620de1142267826ff43d20e4b5a7ba376736
Dasol Park <dasol.park615@gmail.com>
2020-08-18 22:51:15 +0900

    Array Types

--- app.ts
+++ app.ts
@@ -1,11 +1,21 @@
-const person: {
-  name: string;
-  age: number;
-} = {
-  // This is just TypeScript's representation of an object type that helps TypeScript understand objects are working with
-  // const person = { // This is the better syntax code
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+// This is just TypeScript's representation of an object type that helps TypeScript understand objects are working with
+const person = {
+  // This is the better syntax code
   name: 'Maximilian',
   age: 30,
+  hobbies: ['Sports', 'Cooking'],
 };
 
+let favoriteActivities: string[]; // any[]
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase()); // string이기 때문에 string 관련 메소드 사용 가능
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
